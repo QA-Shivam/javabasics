38e05872833874df142e5c2c21efa8d90ce0f727
QA-Shivam <shivamkumar16399@gmail.com>
2026-01-24 09:20:28 +0530

    java-string

---
+++ src/string/reverseString2.java
@@ -0,0 +1,8 @@
+package string;
+
+public class reverseString2 {
+    public static void main(String[] args) {
+        StringBuilder str= new StringBuilder("Shivam@ybm");
+        System.out.println(str.reverse());
+    }
+}
