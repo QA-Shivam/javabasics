c357af1c36c6a56bca3eca088683e1e85ae03dd1
QA-Shivam <shivamkumar16399@gmail.com>
2026-02-17 22:16:35 +0530

    java-array

--- src/Array/FormLargestNumber.java
+++ src/Array/FormLargestNumber.java
@@ -1,8 +1,17 @@
 package Array;
 
+import java.util.Arrays;
+
 public class FormLargestNumber {
 
+    public static String FormLargestNumber(int arr[]){
+        String [] strArr= Arrays.stream(arr).mapToObj(String::valueOf).toArray(String[]::new);
+        Arrays.sort(strArr,(a,b)->(b+a).compareTo(a+b));
+        return  String.join("",strArr);
+    }
     public static void main(String[] args) {
+        int arr[]={2,3,43};
+        System.out.println(FormLargestNumber(arr));
 
     }
 }
