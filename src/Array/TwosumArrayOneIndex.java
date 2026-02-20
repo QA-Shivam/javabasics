297b903a78d260e5547bca81d9f7fe38d32aafa2
QA-Shivam <shivamkumar16399@gmail.com>
2026-02-20 22:37:50 +0530

    java-array

---
+++ src/Array/TwosumArrayOneIndex.java
@@ -0,0 +1,27 @@
+package Array;
+
+import java.util.Arrays;
+
+public interface TwosumArrayOneIndex {
+
+    public static int[] TwosumArrayOneIndex(int[] arr,int traget ){
+        int right=0;
+        int left=arr.length-1;
+        while (right<left){
+            int sum=arr[left]+arr[right];
+            if(sum<traget){
+                right++;
+            }else if(sum>traget){
+                left--;
+            }else {
+                return new int[]{ right + 1,left+1};
+            }
+        }
+        return new int[]{};
+    }
+    public static void main(String[] args) {
+        int [] arr={2,4,5,6,9};
+        int target=10;
+        System.out.println(Arrays.toString(TwosumArrayOneIndex(arr,target)));
+    }
+}
