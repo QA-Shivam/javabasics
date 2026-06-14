c7f8787249f90bddb2c4df330bd54bae5549c9e6
QA-Shivam <shivamkumar16399@gmail.com>
2026-06-15 00:06:45 +0530

    practice

---
+++ src/NumberPatterns/Pattern4.java
@@ -0,0 +1,24 @@
+package NumberPatterns;
+
+import java.util.Scanner;
+
+public class Pattern4 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        for (int i=n;i>=1;i--){
+            for (int j=1;j<=i;j++){
+                System.out.print(" "+i);
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+         5 5 5 5 5
+         4 4 4 4
+         3 3 3
+         2 2
+         1
+*/
---
+++ src/NumberPatterns/Pattern8.java
@@ -0,0 +1,24 @@
+package NumberPatterns;
+
+import java.util.Scanner;
+
+public class Pattern8 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        int num=1;
+        for (int i=n;i>=1;i--){
+            for (int j=1;j<=i;j++){
+                System.out.print(" "+num);
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+         1 2 3 4
+         5 6 7
+         8 9
+         10
+*/
