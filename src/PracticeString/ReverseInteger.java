23616768c6e2e9373c043adf94c09ac285578c04
QA-Shivam <shivamkumar16399@gmail.com>
2026-06-26 18:41:44 +0530

    java-array

--- src/PracticeString/ReverseInteger.java
+++ src/PracticeString/ReverseInteger.java
@@ -4,12 +4,13 @@ public class ReverseInteger {
     public static int revInt(int x){
       int rev=0;
       while (x!=0){
-          int ld= x%10;
+          int ld= x%10; //3 2 1
+         // This condition is trying to prevent integer overflow.
           if (rev<Integer.MIN_VALUE/10 || rev>Integer.MAX_VALUE/10){
               return 0;
           }
-          rev=(rev*10)+ld;
-          x=x/10;
+          rev=(rev*10)+ld; //3 32 321
+          x=x/10; // 12 1 0
       }
       return rev;
     }
