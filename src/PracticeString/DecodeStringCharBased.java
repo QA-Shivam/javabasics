074ae09d0d57d2960ebaa205b9640daf61dc5202
QA-Shivam <shivamkumar16399@gmail.com>
2026-07-01 08:03:55 +0530

    practice

---
+++ src/PracticeString/DecodeStringCharBased.java
@@ -0,0 +1,27 @@
+package PracticeString;
+
+public class DecodeStringCharBased {
+    public static String decode(String str){
+        StringBuilder result=new StringBuilder();
+        StringBuilder letters= new StringBuilder();
+        for (char c:str.toCharArray()){
+            if (Character.isLetter(c)){
+                letters.append(c);
+            } else if (Character.isDigit(c)) {
+                int count =c-'0';
+                result.append(letters);
+                char lastchar=letters.charAt(letters.length()-1);
+                for (int i=1;i<count;i++){
+                    result.append(lastchar);
+                }
+                letters.setLength(0);
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        String s="sh2i1ve2";
+        System.out.println(decode(s));
+    }
+}
--- src/PracticeString/DecodeStringGroupBased.java
+++ src/PracticeString/DecodeStringGroupBased.java
@@ -2,20 +2,20 @@ package PracticeString;
 
 public class DecodeStringGroupBased {
     public static String deccode(String str){
-    StringBuilder result= new StringBuilder();
-    StringBuilder letters=new StringBuilder();
-    for (char ch:str.toCharArray()){
-        if (Character.isLetter(ch)){
-            letters.append(ch);
-        } else if (Character.isDigit(ch)) {
-            int count =ch-'0';
-            for (int i=0;i<count;i++){
-                result.append(letters);
+        StringBuilder result= new StringBuilder();
+        StringBuilder letters=new StringBuilder();
+        for (char c: str.toCharArray()){
+            if (Character.isLetter(c)){
+                letters.append(c);
+            } else if (Character.isDigit(c)) {
+                int count= c-'0';
+                for (int i=0;i<count;i++){
+                    result.append(letters);
+                }
+                letters.setLength(0);
             }
-            letters.setLength(0); // empty the group
         }
-    }
-    return result.toString();
+        return result.toString();
     }
 
     public static void main(String[] args) {
