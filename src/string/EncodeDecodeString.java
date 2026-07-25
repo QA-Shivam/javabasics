6208b39f0a1c68fb3543a773dcc41bc2d5f7f66e
QA-Shivam <shivamkumar16399@gmail.com>
2026-07-25 12:52:05 +0530

    practice

--- src/string/EncodeDecodeString.java
+++ src/string/EncodeDecodeString.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 class EncodeDecodeString {
+//    Design an algorithm to encode a list of strings to a string.
+//    The encoded string is then sent over the network and is decoded back to the original list of strings.
+
 
     public static String encode(List<String> strs) {
         StringBuilder encodeStr= new StringBuilder();
