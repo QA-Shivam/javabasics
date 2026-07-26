50693ecb46c9de752a73dc1ba2e15dc22ef54cea
QA-Shivam <shivamkumar16399@gmail.com>
2026-07-26 08:50:27 +0530

    practice

---
+++ src/Array/findKthLargestUsingSort.java
@@ -0,0 +1,17 @@
+package Array;
+
+import java.util.Arrays;
+
+public class findKthLargestUsingSort {
+    public static int kthLargest(int []nums,int k){
+        Arrays.sort(nums);
+        return nums[nums.length-k];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,2,1,5,6,4} ;
+        int k = 2;
+        System.out.println(kthLargest(nums,k));
+    }
+
+}
