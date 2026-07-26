e81e5607ec121ce78bb3f319233c4e6845dc90c0
QA-Shivam <shivamkumar16399@gmail.com>
2026-07-26 10:18:41 +0530

    practice

---
+++ src/Array/findKthLargestQuickSelect.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class findKthLargestQuickSelect {
+    public static int findKthLargestQuickSelect(int []nums, int k){
+        return 0;
+    }
+    public static void main(String[] args) {
+
+    }
+}
--- src/PracticeString/DecodeString.java
+++ src/PracticeString/DecodeString.java
@@ -1,19 +1,54 @@
 package PracticeString;
 
+import java.util.Stack;
+
 public class DecodeString {
 
-    public static void main(String[] args) {
-        String s = "3[a]2[bc]";
-        StringBuilder letters=new StringBuilder();
-        StringBuilder result= new StringBuilder();
-        for (char c: s.toCharArray()){
-            if (Character.isLetter(c)){
-                letters.append(c);
-            } else if (Character.isDigit(c)) {
-                int count= c-'0';
-                result.append(letters);
+    public static String decodeString(String str) {
+        Stack<Integer> numberStack = new Stack<>();
+        Stack<String> stringStack = new Stack<>();
+        StringBuilder result = new StringBuilder();
 
+        //Stores the current number.
+        int currentNumber = 0;
+        for (char c : str.toCharArray()) {
+            if (Character.isDigit(c)) {
+                currentNumber = (currentNumber * 10) + (c - '0');
+                continue;
             }
+            if (c == '[') {
+                numberStack.push(currentNumber);
+                currentNumber = 0;
+                stringStack.push(String.valueOf(c));
+                continue;
+            }
+            if (c != ']') {
+                stringStack.push(String.valueOf(c));
+                continue;
+            }
+            StringBuilder temp = new StringBuilder();
+            while (!stringStack.peek().equals("[")) {
+                temp.insert(0, stringStack.pop());
+            }
+            // remove [ bracket
+            stringStack.pop();
+
+            StringBuilder replacement = new StringBuilder();
+            int count = numberStack.pop();
+            for (int i = 0; i < count; i++) {
+                replacement.append(temp);
+            }
+            stringStack.push(replacement.toString());
         }
+
+        while (!stringStack.empty()) {
+            result.insert(0, stringStack.pop());
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "3[ab]2[bc]";
+        System.out.println(decodeString(s));
     }
 }
